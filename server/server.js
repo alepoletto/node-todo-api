5fd0bca08abd2f0b6bb00bc67389a4861a6b8d81
Alexandre Poletto <alexandrepoletto@gmail.com>
2017-02-21 11:04:27 -0300

    test post/todos route

--- server/server.js
+++ server/server.js
@@ -14,9 +14,9 @@ app.post('/todos', (req, res) => {
     text: req.body.text
   });
 
-  todo.save((doc) => {
+  todo.save().then((doc) => {
     res.send(doc);
-  }), (e) => {
+  }, (e) => {
     res.status(400).send(e);
   })
 });
@@ -24,3 +24,6 @@ app.post('/todos', (req, res) => {
 app.listen(3000, () => {
   console.log('started on port 3000');
 });
+
+
+module.exports = { app };
